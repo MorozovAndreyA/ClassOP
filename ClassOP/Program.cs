23d8fbe7d9d56b601a970e0a664adda2388fa362
MorozovAndreyA <42968272+MorozovAndreyA@users.noreply.github.com>
2019-05-21 08:59:45 +0300

    Добавьте файлы проекта.

---
+++ ClassOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
